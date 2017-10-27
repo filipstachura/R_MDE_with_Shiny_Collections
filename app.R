9e1b143b43360412b8649eb233eab7a17e9868d6
tinu-schneider <ti.s@bluewin.ch>
2017-10-27 18:21:09 +0200

    I give up for the moment;
    db gets updated, but editors not always. it works as long as the input comes from one instance only.
    it fails, if we use more than one editors to write to the db

--- app.R
+++ app.R
@@ -8,36 +8,13 @@ library(dplyr)
 # devtools::install_github("Appsilon/shiny.collections")
 library(shiny.collections)
 
-## TODO
-# reset text
-# back/forward to line x
-
-
-# helper: template as first argument to place the 
-# content into the template
-.replace_placeholder <- function(template, content, placeholder) {
-    sub(pattern = placeholder, replacement = content, x = template)
-}
-
-# helper: get last value from the db
-.get_last_value_in_db_from_db <- function(coll) {
-    db <- coll$collection 
-    
-    # if a new db starts up, it will be empty
-    if (nrow(db) < 1) return(" ")
-    
-    res <- db %>% arrange(time) %>% filter(row_number() == n()) %>% select(text)
-    return(as.character(res))
-}
-
-
-# Start `rethinkdb.exe`
+# Start `rethinkdb.exe` on windows; we assume it is in the same dir like the app
 system(paste0(getwd(), "/rethinkdb.exe"), wait = FALSE)
 
 # Start a connection to the db
 connection <- shiny.collections::connect() 
 
-# Our dummy-template. Later we will replace 'intro' with the content from the editor.
+# Our dummy-template. Later we will replace '---intro---' with the content from the editor.
 template <- c("Your text from the editor fits into the template:
               <br> ---intro--- <br>
               Great! And here ends the template.")
@@ -48,12 +25,10 @@ jsCode  <- '
         shinyjs.get_editor_text = function() {
             Shiny.onInputChange("textfield", intro_text_MDE.value());
         }
-        shinyjs.start_editor = function(initial_value) {
+        shinyjs.start_editor = function() {
             intro_text_MDE = new SimpleMDE({
                 element: document.getElementById("intro"),
-    	        initialValue: initial_value[0],
-                spellChecker: false, 
-                autosave: {enabled: false}
+                spellChecker: false
             });
         }
         shinyjs.update_editor = function(content) {
@@ -65,7 +40,7 @@ jsCode  <- '
 ui <- fluidPage(
     useShinyjs(),
     extendShinyjs(text = jsCode),    
-    tags$head(HTML('<link rel="stylesheet" type="text/css" href="simplemde.min.css">')),
+    tags$head(HTML('<link rel="stylesheet" type="text/css" href="simplemde.min.css">')), # is in www/
     tags$style(HTML("
                     body { margin: 20px;}
                     .CodeMirror, .CodeMirror-scroll {
@@ -78,25 +53,24 @@ ui <- fluidPage(
     fluidRow(
         h1("Markdown Editor"),
         h4("Enter some Text"),
-        helpText("It will be integrated into the template."),
         HTML('<textarea id="intro" rows="4" cols="75"></textarea>'),
-        # textAreaInput("intro", "", cols = "75"),
         actionButton("apply_changes_button", label = "Apply Changes"),
         hr(), 
         h3("The final text"), 
-        helpText("This text will be part of  a .Rmd report."), 
         htmlOutput("introtext"),
         hr(),
         h4("Raw content from the db"),
+        verbatimTextOutput("session_token"),
         verbatimTextOutput("content_db"),
         hr(),
         HTML('<div class="footer">2017, Tinu Schneider, 
-                <a href="mailto:tinu@tinuschneider.ch">tinu@tinuschneider.ch</a>
+                <a href="mailto:tinu@tinuschneider.ch">tinu@tinuschneider.ch</a>, 
+                Editor: <a href="https://simplemde.com/" target="_blank">SimpleMDE</a>
              </div>'),
-        HTML('<div class="footer">Editor: <a href="https://simplemde.com/" target="_blank">SimpleMDE</a></div>'),
-        HTML('<script src="simplemde.min.js"></script>'),
+        # start simplemde
+        HTML('<script src="simplemde.min.js"></script>'), # is in www/
         HTML('<script>
-             var intro_text_MDE; // init SimpleMDE
+                var intro_text_MDE; // init SimpleMDE globally
              </script>'
              )
         )
@@ -113,21 +87,32 @@ server <- shinyServer(
         
         the_content <- shiny.collections::collection("content", connection)
         
-        # get the value from the editor as reactive
+        # get the value from the editor as reactive, return a string
         value_in_editor <- reactive({
             value <- input$textfield
             if (is.null(value)) value <- " "
             list(value = value)
         })
         
-        # get the last value in the db as reactive
+        # get the last value in the db as reactive, return a string
         last_value_in_db <- reactive({
-            list(value = .get_last_value_in_db_from_db(the_content))
+            db <- the_content$collection 
+            
+            # if a new empty db starts up, it will be a 0x0 tibble
+            if (nrow(db) < 1)  {
+                res <- " "
+            } else {
+                res <- db %>% 
+                        arrange(time) %>% 
+                        filter(row_number() == n()) %>% 
+                        select(text) %>% 
+                        as.character()
+            }
+            return(list(value = res))
         })
         
-        # start the editor with last entry from db
-        startvalue <- isolate(.get_last_value_in_db_from_db(the_content))
-        js$start_editor(startvalue)
+        # start the editor 
+        js$start_editor()
         
         # why do we need this? init the js?
         js$get_editor_text()
@@ -135,56 +120,61 @@ server <- shinyServer(
         # Button clicked
         onclick("apply_changes_button", {
             js$get_editor_text() # refresh
-            who_clicked$token <- session$token
-            who_clicked$clicked <- TRUE
-            # print(who_clicked$token)
+            who_clicked$token   <- session$token
+            who_clicked$clicked <- TRUE # then write to db
         })
         
-        # update the db AFTER the button is clicked
+        # update the db if text in the edotor changed and the button is clicked
         observe({
             actual <- value_in_editor()$value
             last   <- last_value_in_db()$value
 
-            # write to db if content in the editor changed, 
+            # write to db if content in the editor changed
             if (actual != last) {
                 if (session$token == who_clicked$token & who_clicked$clicked) {
                     shiny.collections::insert(the_content, list(
-                            who  = session$token,
-                            time = lubridate::now(), 
-                            text = actual) )
+                            token = session$token,
+                            time  = lubridate::now(), 
+                            text  = actual) )
                 } 
             } 
             who_clicked$clicked <- FALSE
         })
         
-        
-        
-        # update the other editors
+
+        # update the other editors; works on startup, too
+        # BUT FAILS, if we have more than one browser instance and change the 
+        # input from one to the other. So the whole app is basically useless...
         observe({
             last <- last_value_in_db()$value
             if (session$token != who_clicked$token ) {
                 js$update_editor(last)
             }
+            js$get_editor_text()
         })
         
-        # observe({
-        #     value_in_editor()$value
-        #     js$get_editor_text()
-        # })
+        # show the session token 
+        output$session_token <- renderText({
+            paste("This session token: ", session$token)
+        })
         
+        # put the text from the editor into the template
+        # works fine through with multiple instances
         output$introtext <- renderText({
             # read from db and insert the content into the template
             content_to_insert <- last_value_in_db()$value
-            .replace_placeholder(template, content_to_insert, "---intro---")            
+            sub(pattern = "---intro---", replacement = content_to_insert, x = template)
         })
         
         # show the content in the db (or part of it)
         output$content_db <- renderPrint({
-            the_content$collection %>% select(who,  text, time) %>% arrange(desc(time))
+            if (nrow(the_content$collection) < 1) {
+                "db is empty -- nothing to show"
+            } else {
+                the_content$collection %>% select(token,  text, time) %>% arrange(desc(time))
+            }
         })
-        
 })
     
-
 
 shinyApp(ui = ui, server = server)
